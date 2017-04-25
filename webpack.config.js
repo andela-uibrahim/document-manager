fc14969f74603b77baf6a2f110a9fcd0005048b3
andela-uibrahim <usman.ibrahim@andela.com>
2017-04-25 12:19:57 +0100

    chore(deployment): deploying app to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+//   template: './client/src/index.html',
+//   filename: 'index.html',
+//   inject: 'body'
+// });
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  entry: [
-    './client/src/index.jsx'
-  ],
+  entry: './client/src/index.jsx',
   
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react']
+          presets: ['react', 'es2015']
         }
       },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
-      { 
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/ 
-      },
+      // { 
+      //   test: /\.jsx$/,
+      //   loader: 'babel-loader',
+      //   exclude: /node_modules/ 
+      // },
       { 
         test: /\.css$/,
         loader: ['style-loader','css-loader'] 
@@ -49,19 +47,19 @@ module.exports = {
     ]
   },
   target: 'web',
-  resolve: {
-    extensions: ['.js']
-  },
+  // resolve: {
+  //   extensions: ['.js']
+  // },
   output: {
-    path: path.resolve('client/dist'),
+    path: path.resolve('./client/dist'),
     filename: 'bundle.js'
   },
-  devServer: {
-    contentBase: './client/dist',
-    hot: true
-  },
-  plugins: [
-    HtmlWebpackPluginConfig,
-    new ExtractTextPlugin('stylesheet.css')
-  ]
+  // devServer: {
+  //   contentBase: './client/dist',
+  //   hot: true
+  // },
+  // plugins: [
+  //   HtmlWebpackPluginConfig,
+  //   new ExtractTextPlugin('stylesheet.css')
+  // ]
 };
