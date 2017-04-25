b8bc576f3bd68bebba8be85bd746e9fb57fdd25b
andela-uibrahim <usman.ibrahim@andela.com>
2017-04-25 10:07:49 +0100

    chore(deploy): deploy app to heroku #2

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const configuration = {
+  production:{
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    log: false
+  },
   development: {
     url: process.env.DEV_DBURL,
     dialect: 'postgres',
