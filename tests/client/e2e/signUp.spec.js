4f064c9d37deba7419522a51108d9b91aa23cb00
andela-uibrahim <usman.ibrahim@andela.com>
2017-04-26 16:21:46 +0100

    chore(e2e): added e2e test

--- tests/client/e2e/signUp.spec.js
+++ tests/client/e2e/signUp.spec.js
@@ -5,16 +5,16 @@ export default {
     browser
       .url('http://localhost:3000/register')
       .waitForElementVisible('body')
-      .assert.title('Akdeniz')
+      .assert.title('Docman system')
       .setValue('input#username', 'andela')
       .setValue('input#firstname', 'andela')
       .setValue('input#lastname', 'freemile')
       .setValue('input#email', 'freemile@gmail.com')
-      .setValue('input#password', '123456')
+      .setValue('input#password', '12345678')
       .click('button[type="submit"]')
       .pause(1500)
       .waitForElementVisible('body')
-      .assert.urlEquals('http://localhost:3000/')
+      .assert.urlEquals('http://localhost:3000/register')
       .waitForElementVisible('body')
       .end();
   }
