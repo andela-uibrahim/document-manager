8b98d9bd8a9e6750052171fe09ebe20f64083706
andela-uibrahim <usman.ibrahim@andela.com>
2017-05-09 01:02:50 +0100

    bugfix(Heroku): fixed app issues on heroku #3

--- client/src/components/document/MyDocuments.jsx
+++ client/src/components/document/MyDocuments.jsx
@@ -78,56 +78,58 @@ import verifyToken from '../../actions/authentication/verifyToken';
           : <div className= "row">
               <div className="col s2 m2">
               </div>
-              <div className="col s10 m10 preloader-wrapper big active">
-                <div className="spinner-layer spinner-blue">
-                  <div className="circle-clipper left">
-                    <div className="circle"></div>
+              <div className="col s6 m6">
+                <div className="preloader-wrapper big active">
+                  <div className="spinner-layer spinner-blue">
+                    <div className="circle-clipper left">
+                      <div className="circle"></div>
+                    </div>
+                    <div className="gap-patch">
+                      <div className="circle"></div>
+                    </div>
+                    <div className="circle-clipper right">
+                      <div className="circle"></div>
+                    </div>
                   </div>
-                  <div className="gap-patch">
-                    <div className="circle"></div>
-                  </div>
-                  <div className="circle-clipper right">
-                    <div className="circle"></div>
-                  </div>
-                </div>
 
-                <div className="spinner-layer spinner-red">
-                  <div className="circle-clipper left">
-                    <div className="circle"></div>
+                  <div className="spinner-layer spinner-red">
+                    <div className="circle-clipper left">
+                      <div className="circle"></div>
+                    </div>
+                    <div className="gap-patch">
+                      <div className="circle"></div>
+                    </div>
+                    <div className="circle-clipper right">
+                      <div className="circle"></div>
+                    </div>
                   </div>
-                  <div className="gap-patch">
-                    <div className="circle"></div>
-                  </div>
-                  <div className="circle-clipper right">
-                    <div className="circle"></div>
-                  </div>
-                </div>
 
-                <div className="spinner-layer spinner-yellow">
-                  <div className="circle-clipper left">
-                    <div className="circle"></div>
+                  <div className="spinner-layer spinner-yellow">
+                    <div className="circle-clipper left">
+                      <div className="circle"></div>
+                    </div>
+                    <div className="gap-patch">
+                      <div className="circle"></div>
+                    </div>
+                    <div className="circle-clipper right">
+                      <div className="circle"></div>
+                    </div>
                   </div>
-                  <div className="gap-patch">
-                    <div className="circle"></div>
-                  </div>
-                  <div className="circle-clipper right">
-                    <div className="circle"></div>
-                  </div>
-                </div>
 
-                <div className="spinner-layer spinner-green">
-                  <div className="circle-clipper left">
-                    <div className="circle"></div>
-                  </div><
-                  div className="gap-patch">
-                    <div className="circle"></div>
-                  </div>
-                  <div className="circle-clipper right">
-                    <div className="circle"></div>
+                  <div className="spinner-layer spinner-green">
+                    <div className="circle-clipper left">
+                      <div className="circle"></div>
+                    </div><
+                    div className="gap-patch">
+                      <div className="circle"></div>
+                    </div>
+                    <div className="circle-clipper right">
+                      <div className="circle"></div>
+                    </div>
                   </div>
                 </div>
               </div>
-              </div>}
+            </div>}
       </div>
     );
   }
